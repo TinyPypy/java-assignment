917e99743cfbf0ced7e8d49c332b248bf63f187a
TinyPypy <munenejoypatience@gmail.com>
2022-10-16 14:16:20 +0000

    first commit

---
+++ TryingMethods.java
@@ -0,0 +1,34 @@
+public class TryingMethods{
+    // this class is literally me trying out if i got shit in class. hehe!
+    public static void main(String [] args) {
+        String Day;
+        mySchedule("Monday");
+        mySchedule("Tuesday");
+        mySchedule("wednesday");
+        mySchedule("Thursday");
+        mySchedule("Friday");
+        mySchedule("Saturday");
+        mySchedule("Sunday");
+
+        
+
+
+        
+    }
+
+
+    public static void mySchedule(String Day) {
+        System.out.println(Day);}
+
+        public static  void calculations(int t) {
+
+            int t = 1-1000
+            
+            
+        }
+                
+
+
+        
+    }
+}
