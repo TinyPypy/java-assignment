4fe54bb98b80d857626df9a80d760278da7837ee
Munene Patience <114987649+TinyPypy@users.noreply.github.com>
2022-10-19 18:16:34 +0000

    My schedule branch created

--- MySchedule.java
+++ MySchedule.java
@@ -4,32 +4,24 @@ public class MySchedule {
         switch (Day) {
             case 1:
                 myday = "Monday";
-
                 break;
             case 2:
                 myday = "Tuesday";
-
                 break;
-
             case 3:
                 myday = "Wednesday";
-
                 break;
             case 4:
                 myday = "Thursday";
-
                 break;
             case 5:
                 myday = "Friday";
-
                 break;
             case 6:
                 myday = "Saturday";
-
                 break;
             case 7:
                 myday = "Sunday";
-
             default:
                 break;
     
